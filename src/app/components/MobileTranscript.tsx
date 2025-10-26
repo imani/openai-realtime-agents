46d1dfa727da7872d4fc4f8c3f9c99f694f8088f
Majid Ebrahimi <116424340+Majid-Ebrahimi@users.noreply.github.com>
2025-10-26 16:43:00 +0330

    [Fix],[NT-2275] Some changes

--- src/app/components/MobileTranscript.tsx
+++ src/app/components/MobileTranscript.tsx
@@ -459,7 +459,7 @@ function MobileTranscript({
               <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z" />
               <path d="M19 11a1 1 0 0 0-2 0 5 5 0 0 1-10 0 1 1 0 0 0-2 0 7 7 0 0 0 6 6.92V21a1 1 0 1 0 2 0v-3.08A7 7 0 0 0 19 11z" />
             </svg>
-            {isSpeechSupported
+            {isSpeechSupported && canSend
               ? "شروع گفت‌وگو صوتی"
               : "گفتگوی صوتی پشتیبانی نمی‌شود"}
           </button>
